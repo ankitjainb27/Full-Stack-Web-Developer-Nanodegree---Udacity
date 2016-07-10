b343507e42eac99365cb4856b1878ac94a3a86c4
Ankit Jain <ankit.jainb27@gmail.com>
2016-07-11 00:39:34 +0530

    documentation added in media.py

--- media.py
+++ media.py
@@ -3,6 +3,13 @@ import webbrowser
 
 # Model for the Movie Trailers
 class Movie():
+    """Movie is the model for Movie Trailer
+    Attributes:
+        title (str): Title of the movie.
+        poster_image (str): link to poster of the movie
+        trailer_youtube_url (str): link to the youtube trailer of the movie
+        year (int): year in which the movie was released
+    """
     def __init__(self, title, poster_image, trailer_youtube_url, year):
         self.title = title
         self.poster_image_url = poster_image
