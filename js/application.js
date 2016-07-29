f3ab466af024a35d3f7074b78220fff7bc468289
Ankit Jain <ankit.jainb27@gmail.com>
2016-07-30 00:37:00 +0530

    foursquare list coming correctly

--- js/application.js
+++ js/application.js
@@ -1,4 +1,5 @@
 var meetupApiUrl = 'http://api.meetup.com/2/open_events?status=upcoming&radius=5&category=34&and_text=False&limited_events=False&text=coding+programming+ruby+python+javascript+html&desc=False&offset=0&photo-host=public&format=json&lat=41.878114&page=100&lon=-87.629798&sig_id=178346822&sig=1a186723262b63d4b2deee474b8d95bc0ec2ec9f';
+var foursquareApiUrl = 'https://api.foursquare.com/v2/venues/search?client_id=TVCLSUOTWTNYYYNQXJLM2CM5LGMUQPNO0RAPW1O5WLMPWCQH%20&client_secret=SCRGCS4SBR5J1IDRRWTKBAUIM31YB4TUKMMRVRJOW13DC1PW%20&v=20130815%20&ll=40.7,-74%20&query=sushi';
 
 String.prototype.contains = function (other) {
     return this.indexOf(other) !== -1;
@@ -56,6 +57,53 @@ var Corner = function (venueObject, map) {
  * @constructor
  * @param {object} meetup - JSON-like meetup from the Meetup open_venue API
  */
+
+var Foursquare = function (meetup, map) {
+    var self = this;
+    // attach venue object
+    self.name = ko.observable(meetup.name);
+    self.location = meetup.location;
+    self.lat = self.location.lat;
+    self.lng = self.location.lng;
+    self.map_location = ko.computed(function () {
+        if (self.lat === 0 || self.lon === 0) {
+            return null;
+        } else {
+            return new google.maps.LatLng(self.lat, self.lon);
+        }
+    });
+    self.formattedAddress = ko.observable(self.location.formattedAddress);
+    self.marker = (function (corner) {
+        var marker;
+
+        if (corner.map_location()) {
+            marker = new google.maps.Marker({
+                position: corner.map_location(),
+                map: map,
+            });
+        }
+
+        return marker;
+    })(self);
+    self.formattedMeetupList = function () {
+        meetupSubstring = '<ul class="info-window-list">';
+        meetupSubstring += '<li>' + '<a href="' + meetup.url() + '">' +
+            meetup.name() +
+            '</a></li>';
+        meetupSubstring += '</ul>';
+        return '<div class="info-window-content">' +
+            '<span class="info-window-header">' + self.name() + '</span>' +
+            '<p>' + self.formattedAddress() + '</p>' +
+            meetupSubstring +
+            '</div>';
+    };
+    // returns if the meetup has a venue that is listed
+    self.id = ko.observable(meetup.id);
+    self.url = ko.observable(meetup.url);
+    console.log(self)
+};
+
+
 var Meetup = function (meetup) {
     var self = this;
 
@@ -87,7 +135,7 @@ var Meetup = function (meetup) {
 /* Represents a Google Map object */
 var GoogleMap = function (center, element) {
     var mapOptions = {
-        zoom: 14,
+        zoom: 13,
         center: center,
         mapTypeControlOptions: {
             mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'usroadatlas']
@@ -109,7 +157,7 @@ var AppViewModel = function () {
 
     function initialize() {
         map = GoogleMap(center, mapCanvas);
-        fetchMeetups(meetupApiUrl);
+        fetchMeetups(foursquareApiUrl);
     }
 
     if (typeof google !== 'object' || typeof google.maps !== 'object') {
@@ -118,7 +166,7 @@ var AppViewModel = function () {
 
     var map,
         mapCanvas = $('#map')[0],
-        center = new google.maps.LatLng(41.8886, -87.6310); // Chicago
+        center = new google.maps.LatLng(40.7, -74); // Chicago
 
     var infoWindow = new google.maps.InfoWindow();
 
@@ -129,17 +177,16 @@ var AppViewModel = function () {
     self.numCorners = ko.observable(0);
 
     self.query = ko.observable('');
-
     self.search = function () {
+        console.log("came");
         // empty function for future functionality, keep present to avoid page reload
     };
-
     self.filteredCornerList = ko.computed(function () {
-        self.cornerList().forEach(function (corner) {
+        self.meetupList().forEach(function (corner) {
             corner.marker.setMap(null);
         });
 
-        var results = ko.utils.arrayFilter(self.cornerList(), function (corner) {
+        var results = ko.utils.arrayFilter(self.meetupList(), function (corner) {
             return corner.name().toLowerCase().contains(self.query().toLowerCase());
         });
 
@@ -158,41 +205,65 @@ var AppViewModel = function () {
 
         map.panTo(corner.marker.position);
 
-        corner.marker.setAnimation(google.maps.Animation.DROP);
-        self.cornerList().forEach(function (old_corner) {
+        corner.marker.setAnimation(google.maps.Animation.BOUNCE);
+        self.meetupList().forEach(function (old_corner) {
             if (corner != old_corner) {
                 old_corner.marker.setAnimation(null);
             }
         });
     };
-
+    console.log("came3");
     function fetchMeetups(url) {
         var data;
 
         // execute JSON-P request
         $.ajax({
-            type: "GET",
-            url: url,
-            timeout: 5000,
-            contentType: "application/json",
-            dataType: "jsonp",
-            cache: false,
-
-            // when done
+            url: 'https://api.foursquare.com/v2/venues/search',
+            dataType: 'json',
+            data: 'client_id=TVCLSUOTWTNYYYNQXJLM2CM5LGMUQPNO0RAPW1O5WLMPWCQH%20&client_secret=SCRGCS4SBR5J1IDRRWTKBAUIM31YB4TUKMMRVRJOW13DC1PW%20&v=20130815%20&ll=40.7,-74%20&query=sushi',
+            async: false,
         }).done(function (response) {
             // pull `results` array from JSON
-            data = response.results;
-
+            data = response.response.venues;
             data.forEach(function (meetup) {
-                self.meetupList.push(new Meetup(meetup));
+                foursquare  = new Foursquare(meetup, map)
+                self.meetupList.push(foursquare);
             });
-
-            extractCorners();
+            self.meetupList().forEach(function (meetup) {
+                if (meetup.map_location()) {
+                    google.maps.event.addListener(meetup.marker, 'click', function () {
+                        self.selectCorner(meetup);
+                    });
+                }
+            });
+            //extractCorners();
 
             // if failed
         }).fail(function (response, status, error) {
             $('#search-summary').text('Meetup data could not load...');
         });
+        /*$.ajax({
+            type: "GET",
+            url: url,
+            timeout: 5000,
+            dataType: "json",
+            cache: false,
+
+            // when done
+        }).done(function (response) {
+            // pull `results` array from JSON
+            data1 = response;
+
+            data1.forEach(function (meetup) {
+                //self.meetupList.push(new Meetup(meetup));
+            });
+
+            //extractCorners();
+
+            // if failed
+        }).fail(function (response, status, error) {
+            $('#search-summary').text('Meetup data could not load...');
+        });*/
     }
 
     /* Parses through the meetupList and extracts Corner objects */
